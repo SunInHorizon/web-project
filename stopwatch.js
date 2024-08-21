adf2d71a405978f284e30cb5a21a949c96bcd284
Adithyakiran M Kurup <adithyakiranmkurup@gmail.com>
2024-08-21 20:46:40 +0530

    initial commit

---
+++ stopwatch.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    
+    var minute = 0;
+    var second = 0;
+    var millisecond = 0;
+    var appendmillisecond = document.getElementById('millisecond');
+    var appendsecond = document.getElementById('second');
+    var appenendminute = document.getElementById('minute');
+    var buttonStart = document.getElementById('button-start');
+    var buttonstop = document.getElementById('button-stop');
+    var buttonreset = document.getElementById('button-reset');
+}
