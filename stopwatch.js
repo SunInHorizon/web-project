c9045ee093dc9232af334cf99d2ac8b35caf59dd
Adithyakiran M Kurup <adithyakiranmkurup@gmail.com>
2024-09-06 10:35:32 +0530

    sixth commit

--- stopwatch.js
+++ stopwatch.js
@@ -3,15 +3,28 @@ window.onload = function () {
     var minute = 0;
     var second = 0;
     var millisecond = 0;
+    var minute1 = 0;
+    var second1 = 0;
+    var millisecond1 = 0;
+    var minute2 = 0;
+    var second2 = 0;
+    var millisecond2 = 0;
+    var minute3 = 0;
+    var second3 = 0;
+    var millisecond3 = 0;
     var appendmillisecond = document.getElementById('millisecond');
     var appendsecond = document.getElementById('second');
     var appenendminute = document.getElementById('minute');
+    var laps = document.getElementById('laps');
     var buttonStart = document.getElementById('button-start');
+    var buttonLap = document.getElementById('button-lap');
     //var buttonStop = document.getElementById('button-stop');
     var buttonReset = document.getElementById('button-reset');
     var interval;
     var status = false;
     var flag = false;
+    var temp = false;
+    let count = 0;
     buttonReset.disabled = true;
 
     buttonStart.onclick = function() {
@@ -46,6 +59,42 @@ window.onload = function () {
         
     }
 
+    const zeropad = (num) => {
+        return String(num).padStart(2, "0");
+    }
+
+    buttonLap.onclick = function() {
+
+       
+        if(temp === false)
+        {
+            count++;
+            let li = document.createElement("li");
+            li.innerHTML = `${"#" + count} :- ${zeropad(minute1)}:${zeropad(second1)}.${zeropad(millisecond1)} - ${zeropad(minute)}:${zeropad(second)}.${zeropad(millisecond)}`;
+            laps.appendChild(li);
+            laps.scroll({top: laps.scrollHeight, behavior: "smooth"});
+            temp = true;
+            minute2 = minute;
+            second2 = second;
+            millisecond2 = millisecond;
+        }
+        else
+        {
+            count++;
+            minute3 = minute - minute2;
+            second3 = second - second2;
+            millisecond3 = millisecond - millisecond2;
+            let li = document.createElement("li");
+            li.innerHTML = `${"#" + count} :- ${zeropad(minute2)}:${zeropad(second2)}.${zeropad(millisecond2)} - ${zeropad(minute)}:${zeropad(second)}.${zeropad(millisecond)}`;
+            laps.appendChild(li);
+            laps.scroll({top: laps.scrollHeight, behavior: "smooth"});
+            minute2 = minute;
+            second2 = second;
+            millisecond2 = millisecond;
+        }
+       
+    }
+
     /*buttonStop.onclick = function() {
 
         buttonStart.disabled = true;
@@ -77,6 +126,12 @@ window.onload = function () {
         buttonStart.innerText = "Start";
         buttonStart.style.backgroundColor = "#33ff39"
         buttonReset.disabled = true;
+        laps.innerHTML = "";
+        count = 0;
+        temp = false;
+        minute2 = 0;
+        second2 = 0;
+        millisecond2 = 0;
     }
 
     function startTimer()
